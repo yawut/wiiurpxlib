59e2d1cc1d76bca5d08750fddeaf47b3d28695ba
Ash Logan <ash@heyquark.com>
2020-12-28 09:59:33 +1100

    split elf reading into its own function

--- source/wiiurpxtool.cpp
+++ source/wiiurpxtool.cpp
@@ -82,31 +82,7 @@ void writeelf(const Elf32& elf, std::ostream& os) {
 	os.put(0x00);
 }
 
-void relink(Elf32& elf) {
-	//some variables to keep track of the current file offset
-	auto data_start = elf.ehdr.e_shoff + elf.ehdr.e_shnum * elf.ehdr.e_shentsize;
-	auto file_offset = data_start;
-
-	bool first_section = true;
-	for (auto section_index : elf.section_file_order) {
-		auto& section = elf.sections[section_index];
-		auto& shdr = section.hdr;
-
-		if (!shdr.sh_offset) continue;
-
-		//this bit is replicating some interesting quirks of the original tool
-		//I don't like it but I'm not one to argue too much
-		if (!first_section) {
-			file_offset = alignup(file_offset, 0x40);
-		} else first_section = false;
-
-		shdr.sh_offset = file_offset;
-		file_offset += shdr.sh_size;
-	}
-}
-
-std::optional<Elf32> decompress(std::istream& is)
-{
+std::optional<Elf32> readelf(std::istream& is) {
 	Elf32 elf;
 	is_read_advance(elf.ehdr, is);
 	if (memcmp(elf.ehdr.e_ident, "\x7f""ELF", 4) != 0) {
@@ -134,14 +110,14 @@ std::optional<Elf32> decompress(std::istream& is)
 		if (shdr_pad) is.seekg(shdr_pad, std::ios_base::cur);
 	}
 
-	//sort by file offset, so all sections stay in the same order in-file
+	//sort by file offset, so we always seek forwards and maintain file order
 	elf.section_file_order.resize(elf.sections.size());
 	std::iota(elf.section_file_order.begin(), elf.section_file_order.end(), 0);
 	elf.section_file_order.sort([=] (const auto& a, const auto& b) {
 		return elf.sections[a].hdr.sh_offset < elf.sections[b].hdr.sh_offset;
 	});
 
-	//decompress sections
+	//read section data
 	for (auto& section_index : elf.section_file_order) {
 		auto& section = elf.sections[section_index];
 		auto& shdr = section.hdr;
@@ -149,10 +125,54 @@ std::optional<Elf32> decompress(std::istream& is)
 
 		is.seekg(shdr.sh_offset.value());
 
+		//allocate and read the uncompressed data
+		section.data.resize(shdr.sh_size);
+		is.read((char*)section.data.data(), section.data.size());
+		//compute crc, and we're done!
+		section.crc32 = crc32_rpx(
+			section.crc32,
+			section.data.cbegin(),
+			section.data.cend()
+		);
+	}
+
+	return elf;
+}
+
+void relink(Elf32& elf) {
+	//some variables to keep track of the current file offset
+	auto data_start = elf.ehdr.e_shoff + elf.ehdr.e_shnum * elf.ehdr.e_shentsize;
+	auto file_offset = data_start;
+
+	bool first_section = true;
+	for (auto section_index : elf.section_file_order) {
+		auto& section = elf.sections[section_index];
+		auto& shdr = section.hdr;
+
+		if (!shdr.sh_offset) continue;
+
+		//this bit is replicating some interesting quirks of the original tool
+		//I don't like it but I'm not one to argue too much
+		if (!first_section) {
+			file_offset = alignup(file_offset, 0x40);
+		} else first_section = false;
+
+		shdr.sh_offset = file_offset;
+		file_offset += shdr.sh_size;
+	}
+}
+
+void decompress(Elf32& elf) {
+	//decompress sections
+	for (auto& section_index : elf.section_file_order) {
+		auto& section = elf.sections[section_index];
+		auto& shdr = section.hdr;
+		if (!shdr.sh_offset) continue;
+
 		if (shdr.sh_flags & SHF_RPL_ZLIB) {
 			//read in uncompressed size
 			be2_val<uint32_t> uncompressed_sz;
-			is_read_advance(uncompressed_sz, is);
+			memcpy(&uncompressed_sz, section.data.data(), sizeof(uncompressed_sz));
 			//calc compressed size
 			auto compressed_sz = shdr.sh_size - sizeof(uncompressed_sz);
 
@@ -161,86 +181,63 @@ std::optional<Elf32> decompress(std::istream& is)
 			z_stream zstream = { 0 };
 			inflateInit(&zstream);
 
-			auto remaining_sz = compressed_sz;
-			std::array<uint8_t, CHUNK> compressed_chunk;
+			std::vector<uint8_t> uncompressed_data;
+			uncompressed_data.reserve(uncompressed_sz);
+
 			std::array<uint8_t, CHUNK> uncompressed_chunk;
-			section.data.reserve(uncompressed_sz);
 
-			while (remaining_sz) {
-				auto chunk_sz = compressed_chunk.size();
-				if (remaining_sz < chunk_sz) {
-					chunk_sz = remaining_sz;
-				}
+			//pass to zlib
+			zstream.avail_in = section.data.size();
+			zstream.next_in = (Bytef*)section.data.data() + 4;
 
-				//read in compressed data
-				is.read((char*)compressed_chunk.data(), chunk_sz);
-				//get bytes actually read successfully
-				auto compressed_chunk_sz = is.gcount();
-				//pass to zlib
-				zstream.avail_in = compressed_chunk_sz;
-				zstream.next_in = (Bytef*)compressed_chunk.data();
+			//decompress chunk until zlib is happy
+			int zret;
+			do {
+				//reset uncompressed chunk buffer
+				zstream.avail_out = uncompressed_chunk.size();
+				zstream.next_out = (Bytef*)uncompressed_chunk.data();
+				//decompress!
+				zret = inflate(&zstream, Z_NO_FLUSH);
 
-				//decompress chunk until zlib is happy
-				int zret;
-				do {
-					//reset uncompressed chunk buffer
-					zstream.avail_out = uncompressed_chunk.size();
-					zstream.next_out = (Bytef*)uncompressed_chunk.data();
-					//decompress!
-					zret = inflate(&zstream, Z_NO_FLUSH);
-
-					//get size of decompressed data
-					auto uncompressed_chunk_sz = uncompressed_chunk.size() - zstream.avail_out;
-					//copy uncomressed data into section.data
-					std::copy(
-						uncompressed_chunk.cbegin(),
-						std::next(uncompressed_chunk.cbegin(), uncompressed_chunk_sz),
-						std::back_inserter(section.data)
-					);
-					//update running crc
-					section.crc32 = crc32_rpx(
-						section.crc32,
-						uncompressed_chunk.cbegin(),
-						std::next(uncompressed_chunk.cbegin(), uncompressed_chunk_sz)
-					);
-				} while (zstream.avail_out == 0 && zret == Z_OK);
-
-				remaining_sz -= compressed_chunk_sz;
-				printf("did chunk sz %x, %x bytes remaining, output is %x bytes, crc %08x\n", compressed_chunk_sz, remaining_sz, section.data.size(), section.crc32);
-			}
+				//get size of decompressed data
+				auto uncompressed_chunk_sz = uncompressed_chunk.size() - zstream.avail_out;
+				//copy uncomressed data into section.data
+				std::copy(
+					uncompressed_chunk.cbegin(),
+					std::next(uncompressed_chunk.cbegin(), uncompressed_chunk_sz),
+					std::back_inserter(uncompressed_data)
+				);
+				//update running crc
+				section.crc32 = crc32_rpx(
+					section.crc32,
+					uncompressed_chunk.cbegin(),
+					std::next(uncompressed_chunk.cbegin(), uncompressed_chunk_sz)
+				);
+			} while (zstream.avail_out == 0 && zret == Z_OK);
 
 			inflateEnd(&zstream);
+			section.data = std::move(uncompressed_data);
+
 			//we decompressed this section, so clear the flag
 			shdr.sh_flags &= ~SHF_RPL_ZLIB;
 
 			//update other things as needed
 			shdr.sh_size = uncompressed_sz;
-		} else {
-			//allocate and read the uncompressed data
-			section.data.resize(shdr.sh_size);
-			is.read((char*)section.data.data(), section.data.size());
-			//compute crc, and we're done!
-			section.crc32 = crc32_rpx(
-				section.crc32,
-				section.data.cbegin(),
-				section.data.cend()
-			);
 		}
 	}
 
 	//relink elf to adjust file offsets
 	relink(elf);
-
-	return elf;
 }
 
 int main(int argc, char** argv) {
 	printf("hi!\n");
 	std::ifstream infile("test.rpx", std::ios::binary);
-	auto elf = decompress(infile);
+	auto elf = *readelf(infile);
+	decompress(elf);
 
 	std::ofstream outfile("test.d.rpx", std::ios::binary);
-	writeelf(*elf, outfile);
+	writeelf(elf, outfile);
 
 	return 0;
 }
