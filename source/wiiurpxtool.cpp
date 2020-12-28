7c4ca98ea607c3083a5de3ec757e938162605216
Ash Logan <ash@heyquark.com>
2020-12-28 11:04:31 +1100

    add compression, other small tweaks

--- source/wiiurpxtool.cpp
+++ source/wiiurpxtool.cpp
@@ -33,14 +33,9 @@ typedef struct {
 } Elf32;
 
 
-
 void writeelf(const Elf32& elf, std::ostream& os) {
 	//write elf header out
 	os.write((char*)&elf.ehdr, sizeof(elf.ehdr));
-	//pad to 0x40
-	/*auto pad_size = 0x40 - sizeof(elf.ehdr);
-	char pad_bytes[pad_size] = { 0 };
-	os.write(pad_bytes, pad_size);*/
 
 	auto shdr_pad = elf.ehdr.e_shentsize - sizeof(elf.sections[0].hdr);
 
@@ -63,9 +58,12 @@ void writeelf(const Elf32& elf, std::ostream& os) {
 		os.write((const char*)section.data.data(), size);
 	}
 	file_offset += size;
-	//pad end of file to 0x40
-	os.seekp(alignup(file_offset, 0x40) - 1);
-	os.put(0x00);
+	//if file length is not aligned to 0x40...
+	if (file_offset & (0x40 - 1)) {
+		//pad end of file
+		os.seekp(alignup(file_offset, 0x40) - 1);
+		os.put(0x00);
+	}
 }
 
 std::optional<Elf32> readelf(std::istream& is) {
@@ -114,12 +112,6 @@ std::optional<Elf32> readelf(std::istream& is) {
 		//allocate and read the uncompressed data
 		section.data.resize(shdr.sh_size);
 		is.read((char*)section.data.data(), section.data.size());
-		//compute crc, and we're done!
-		section.crc32 = crc32_rpx(
-			section.crc32,
-			section.data.cbegin(),
-			section.data.cend()
-		);
 	}
 
 	return elf;
@@ -150,8 +142,7 @@ void relink(Elf32& elf) {
 
 void decompress(Elf32& elf) {
 	//decompress sections
-	for (auto& section_index : elf.section_file_order) {
-		auto& section = elf.sections[section_index];
+	for (auto& section : elf.sections) {
 		auto& shdr = section.hdr;
 		if (!shdr.sh_offset) continue;
 
@@ -173,7 +164,7 @@ void decompress(Elf32& elf) {
 			std::array<uint8_t, CHUNK> uncompressed_chunk;
 
 			//pass to zlib
-			zstream.avail_in = section.data.size();
+			zstream.avail_in = section.data.size() - 4;
 			zstream.next_in = (Bytef*)section.data.data() + 4;
 
 			//decompress chunk until zlib is happy
@@ -209,6 +200,13 @@ void decompress(Elf32& elf) {
 
 			//update other things as needed
 			shdr.sh_size = uncompressed_sz;
+		} else {
+			//only compute crc
+			section.crc32 = crc32_rpx(
+				section.crc32,
+				section.data.cbegin(),
+				section.data.cend()
+			);
 		}
 	}
 
@@ -216,205 +214,65 @@ void decompress(Elf32& elf) {
 	relink(elf);
 }
 
+void compress(Elf32& elf) {
+	for (auto& section : elf.sections) {
+		auto& shdr = section.hdr;
+		if (!shdr.sh_offset) continue;
+
+		if (shdr.sh_type == SHT_RPL_FILEINFO || shdr.sh_type == SHT_RPL_CRCS ||
+			shdr.sh_flags & SHF_RPL_ZLIB) continue;
+
+		be2_val<uint32_t> uncompressed_sz = (uint32_t)section.data.size();
+
+		z_stream zstream = { 0 };
+		deflateInit(&zstream, LEVEL);
+
+		//pass to zlib
+		zstream.avail_in = section.data.size();
+		zstream.next_in = (Bytef*)section.data.data();
+
+		std::vector<uint8_t> compressed_data;
+		compressed_data.resize(deflateBound(&zstream, zstream.avail_in) + sizeof(uncompressed_sz));
+
+		zstream.avail_out = compressed_data.size() - sizeof(uncompressed_sz);
+		zstream.next_out = (Bytef*)compressed_data.data() + sizeof(uncompressed_sz);
+
+		//given deflateBound, this is guaranteed to succeed
+		int zret = deflate(&zstream, Z_FINISH);
+
+		compressed_data.resize(zstream.total_out + sizeof(uncompressed_sz));
+		memcpy(compressed_data.data(), &uncompressed_sz, sizeof(uncompressed_sz));
+
+		//not really sure how the original tool does this, but it sure does
+		if (compressed_data.size() >= section.data.size()) continue;
+
+		compressed_data.shrink_to_fit();
+		section.data = std::move(compressed_data);
+		shdr.sh_size = (uint32_t)section.data.size();
+
+		shdr.sh_flags |= SHF_RPL_ZLIB;
+
+		std::cout << "zret: " << zret << std::endl;
+	}
+
+	relink(elf);
+}
+
 int main(int argc, char** argv) {
 	printf("hi!\n");
 	std::ifstream infile("test.rpx", std::ios::binary);
 	auto elf = *readelf(infile);
-	decompress(elf);
 
+	decompress(elf);
 	std::ofstream outfile("test.d.rpx", std::ios::binary);
 	writeelf(elf, outfile);
 
+	compress(elf);
+	std::ofstream cotfile("test.c.rpx", std::ios::binary);
+	writeelf(elf, cotfile);
+
 	return 0;
 }
-
-/*int compress(FILE *in, FILE *out)
-{
-	Elf32_Ehdr ehdr;
-	for(u32 i = 0; i < 0x10; i++)
-		ehdr.e_ident[i] = getc(in);
-	tempa_u32 = ehdr.e_ident[0]<<24|ehdr.e_ident[1]<<16|ehdr.e_ident[2]<<8|ehdr.e_ident[3];
-	if(tempa_u32!=0x7F454C46) return -1;
-	fread16_BE(ehdr.e_type, in);
-	if(ehdr.e_type != 0xFE01) return -1;
-	fread16_BE(ehdr.e_machine, in);
-	fread32_BE(ehdr.e_version, in);
-	fread32_BE(ehdr.e_entry, in);
-	fread32_BE(ehdr.e_phoff, in);
-	fread32_BE(ehdr.e_shoff, in);
-	fread32_BE(ehdr.e_flags, in);
-	fread16_BE(ehdr.e_ehsize, in);
-	fread16_BE(ehdr.e_phentsize, in);
-	fread16_BE(ehdr.e_phnum, in);
-	fread16_BE(ehdr.e_shentsize, in);
-	fread16_BE(ehdr.e_shnum, in);
-	fread16_BE(ehdr.e_shstrndx, in);
-	ulg shdr_data_elf_offset = ehdr.e_shoff + ehdr.e_shnum * ehdr.e_shentsize;
-	fwrite(ehdr.e_ident, sizeof(ehdr.e_ident), 1, out);
-	fwrite16_BE(ehdr.e_type, out);
-	fwrite16_BE(ehdr.e_machine, out);
-	fwrite32_BE(ehdr.e_version, out);
-	fwrite32_BE(ehdr.e_entry, out);
-	fwrite32_BE(ehdr.e_phoff, out);
-	fwrite32_BE(ehdr.e_shoff, out);
-	fwrite32_BE(ehdr.e_flags, out);
-	fwrite16_BE(ehdr.e_ehsize, out);
-	fwrite16_BE(ehdr.e_phentsize, out);
-	fwrite16_BE(ehdr.e_phnum, out);
-	fwrite16_BE(ehdr.e_shentsize, out);
-	fwrite16_BE(ehdr.e_shnum, out);
-	fwrite16_BE(ehdr.e_shstrndx, out);
-	fwrite32_BE(0x00000000, out);
-	fwrite32_BE(0x00000000, out);
-	fwrite32_BE(0x00000000, out);
-	ulg crc_data_offset = 0;
-	u32 *crcs = new u32[ehdr.e_shnum];
-	vector< Elf32_Shdr_Sort > shdr_table_index;
-	Elf32_Shdr *shdr_table = new Elf32_Shdr[ehdr.e_shnum];
-	while(ftell(out)<shdr_data_elf_offset) putc(0, out);
-	fseek(in, ehdr.e_shoff, 0);
-	for (u32 i=0; i<ehdr.e_shnum; i++)
-	{
-		crcs[i] = 0;
-		fread32_BE(shdr_table[i].sh_name, in);
-		fread32_BE(shdr_table[i].sh_type, in);
-		fread32_BE(shdr_table[i].sh_flags, in);
-		fread32_BE(shdr_table[i].sh_addr, in);
-		fread32_BE(shdr_table[i].sh_offset, in);
-		fread32_BE(shdr_table[i].sh_size, in);
-		fread32_BE(shdr_table[i].sh_link, in);
-		fread32_BE(shdr_table[i].sh_info, in);
-		fread32_BE(shdr_table[i].sh_addralign, in);
-		fread32_BE(shdr_table[i].sh_entsize, in);
-		if (shdr_table[i].sh_offset != 0)
-		{
-			Elf32_Shdr_Sort shdr_index;
-			shdr_index.index = i;
-			shdr_index.sh_offset = shdr_table[i].sh_offset;
-			shdr_table_index.push_back(shdr_index);
-		}
-	}
-	sort(shdr_table_index.begin(), shdr_table_index.end(), SortFunc);
-	for(vector<Elf32_Shdr_Sort>::iterator shdr_index=shdr_table_index.begin();shdr_index!=shdr_table_index.end();shdr_index++)
-	{
-		pos = shdr_table[shdr_index->index].sh_offset; fseek(in, pos, 0);
-		shdr_table[shdr_index->index].sh_offset = ftell(out);
-		if (((shdr_table[shdr_index->index].sh_type & SHT_RPL_FILEINFO) == SHT_RPL_FILEINFO)||
-			((shdr_table[shdr_index->index].sh_type & SHT_RPL_CRCS) == SHT_RPL_CRCS)||
-			((shdr_table[shdr_index->index].sh_flags & SHF_RPL_ZLIB) == SHF_RPL_ZLIB))
-		{
-			u32 data_size = shdr_table[shdr_index->index].sh_size;
-			u32 block_size = CHUNK;
-			while(data_size>0)
-			{
-				u8 data[CHUNK];
-				block_size = CHUNK;
-				if(data_size<block_size)
-					block_size = data_size;
-				data_size -= block_size;
-				fread(data, 1, block_size, in);
-				fwrite(data, 1, block_size, out);
-				crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], data, block_size);
-			}
-		}
-		else
-		{
-			u32 data_size = shdr_table[shdr_index->index].sh_size;
-			u32 block_size = CHUNK;
-			u32 have;
-			z_stream strm;
-			u8 buff_in[CHUNK];
-			u8 buff_out[CHUNK];
-			strm.zalloc = Z_NULL;
-			strm.zfree = Z_NULL;
-			strm.opaque = Z_NULL;
-			strm.avail_in = 0;
-			strm.next_in = Z_NULL;
-			if(data_size<CHUNK)
-			{
-				block_size = data_size;
-				deflateInit(&strm, LEVEL);
-				strm.avail_in = fread(buff_in, 1, block_size, in);
-				crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], buff_in, block_size);
-				strm.next_in = buff_in;
-				strm.avail_out = CHUNK;
-				strm.next_out = buff_out;
-				deflate(&strm, Z_FINISH);
-				have = CHUNK - strm.avail_out;
-				if(have+4 < block_size)
-				{
-					fwrite32_BE(data_size, out);
-					fwrite(buff_out, 1, have, out);
-					shdr_table[shdr_index->index].sh_size = have+4;
-					shdr_table[shdr_index->index].sh_flags |= SHF_RPL_ZLIB;
-				}
-				else
-					fwrite(buff_in, 1, block_size, out);
-				deflateEnd(&strm);
-			}
-			else
-			{
-				s32 flush = Z_NO_FLUSH;
-				u32 compress_size = 4;
-				fwrite32_BE(data_size, out);
-				deflateInit(&strm, LEVEL);
-				while(data_size>0)
-				{
-					block_size = CHUNK;
-					flush = Z_NO_FLUSH;
-					if(data_size <= block_size)
-					{
-						block_size = data_size;
-						flush = Z_FINISH;
-					}
-					data_size -= block_size;
-					strm.avail_in = fread(buff_in, 1, block_size, in);
-					crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], buff_in, block_size);
-					strm.next_in = buff_in;
-					do{
-						strm.avail_out = CHUNK;
-						strm.next_out = buff_out;
-						deflate(&strm, flush);
-						have = CHUNK - strm.avail_out;
-						fwrite(buff_out, 1, have, out);
-						compress_size += have;
-					}while(strm.avail_out == 0);
-				}
-				deflateEnd(&strm);
-				shdr_table[shdr_index->index].sh_size = compress_size;
-				shdr_table[shdr_index->index].sh_flags |= SHF_RPL_ZLIB;
-			}
-		}
-		while(ftell(out)%0x40!=0) putc(0, out);
-		if((shdr_table[shdr_index->index].sh_type & SHT_RPL_CRCS) == SHT_RPL_CRCS)
-		{
-			crcs[shdr_index->index] = 0;
-			crc_data_offset = shdr_table[shdr_index->index].sh_offset;
-		}
-	}
-	fseek(out, ehdr.e_shoff, 0);
-	for (u32 i=0; i<ehdr.e_shnum; i++)
-	{
-		fwrite32_BE(shdr_table[i].sh_name, out);
-		fwrite32_BE(shdr_table[i].sh_type, out);
-		fwrite32_BE(shdr_table[i].sh_flags, out);
-		fwrite32_BE(shdr_table[i].sh_addr, out);
-		fwrite32_BE(shdr_table[i].sh_offset, out);
-		fwrite32_BE(shdr_table[i].sh_size, out);
-		fwrite32_BE(shdr_table[i].sh_link, out);
-		fwrite32_BE(shdr_table[i].sh_info, out);
-		fwrite32_BE(shdr_table[i].sh_addralign, out);
-		fwrite32_BE(shdr_table[i].sh_entsize, out);
-	}
-
-	fseek(out, crc_data_offset, 0);
-	for (u32 i=0; i<ehdr.e_shnum; i++)
-		fwrite32_BE(crcs[i], out);
-	delete[]crcs;
-	delete[]shdr_table;
-	shdr_table_index.clear();
-	return 0;
-}*/
 
 template <typename InputIterator>
 uint32_t crc32_rpx(uint32_t crc, InputIterator first, InputIterator last) {
